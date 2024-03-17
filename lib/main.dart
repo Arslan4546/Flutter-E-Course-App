e7cdae1b5e42d48b94aa7f1d249af40cc9775d6b
Arslan Tariq <arslantariq4546@gmail.com>
2024-03-17 09:48:27 +0500

    update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:online_course_app/screens/screen1.dart';
-import 'package:online_course_app/screens/loading_screen.dart';
-import 'package:online_course_app/screens/screen10.dart';
-import 'package:online_course_app/screens/screen11.dart';
-import 'package:online_course_app/screens/screen12.dart';
-import 'package:online_course_app/screens/screen13.dart';
-import 'package:online_course_app/screens/screen2.dart';
-import 'package:online_course_app/screens/screen3.dart';
-import 'package:online_course_app/screens/screen4.dart';
-import 'package:online_course_app/screens/screen5.dart';
-import 'package:online_course_app/screens/screen6.dart';
-import 'package:online_course_app/screens/screen7.dart';
-import 'package:online_course_app/screens/screen8.dart';
-import 'package:online_course_app/screens/screen9.dart';
+import 'package:online_course_app/routes.dart';
 void main(){
-  runApp(const MyApp());
+  runApp(const ECourseApp());
 }
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class ECourseApp extends StatefulWidget {
+  const ECourseApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<ECourseApp> createState() => _ECourseAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _ECourseAppState extends State<ECourseApp> {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        "/": (context) => const LoadingScreen(),
-        "/screen1": (context) => const Screen1(),
-        "/screen2": (context) => const Screen2(),
-        "/screen3": (context) => const Screen3(),
-        "/screen4": (context) => const Screen4(),
-        "/screen5": (context) => const Screen5(),
-        "/screen6": (context) => const Screen6(),
-        "/screen7": (context) => const Screen7(),
-        "/screen8": (context) => const Screen8(),
-        "/screen9": (context) => const Screen9(),
-        "/screen10": (context) => const Screen10(),
-        "/screen11": (context) => const Screen11(),
-        "/screen12": (context) => const Screen12(),
-        "/screen13": (context) => const Screen13(),
-
-
-      }
+   return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes:routes
     );
   }
 }
+
